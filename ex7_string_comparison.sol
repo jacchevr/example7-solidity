0f7056b9705b894dee6563736395407b79d13d97
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-22 11:04:40 -0600

    Update ex7_string_comparison.sol

--- ex7_string_comparison.sol
+++ ex7_string_comparison.sol
@@ -1,1 +1,10 @@
-# example7-solidity
+pragma solidity ^0.4.21;
+
+contract StringComparisson {
+    string myString = "abc";
+    
+    function isSimilar(string compareString) public view returns (bool) {
+        return keccak256(myString) == keccak256(compareString);
+    }
+    
+}
